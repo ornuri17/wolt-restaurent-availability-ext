681cef805bf7eaee3d5c7b19b2277ae45531a4fe
ornuri17 <ornuri17@gmail.com>
2021-12-28 17:46:01 +0200

    First commit

---
+++ content_script.js
@@ -0,0 +1,54 @@
+window.onload = () => {
+  console.log("Wolt-Chrome-Extension Content Script Loaded");
+};
+
+chrome.runtime.onConnect.addListener((port) => {
+  port.onMessage.addListener((message) => {
+    if (message.restaurants) {
+      const restaurants = message.restaurants;
+      let popup = document.getElementById("wolt-chrome-extension-popup");
+      if (popup) {
+        document.body.removeChild(popup);
+      }
+      // Creating popup element
+      popup = document.createElement("div");
+      popup.setAttribute("id", "wolt-chrome-extension-popup");
+      popup.style.width = "100%";
+      popup.style.height = "3em";
+      popup.style.position = "sticky";
+      popup.style.top = "0";
+      popup.style.backgroundColor = "#009de0";
+      popup.style.color = "#FFF";
+      popup.style.fontFamily =
+        'system-ui,-apple-system,BlinkMacSystemFont,"Roboto","Open Sans",sans-serif;';
+      popup.style.zIndex = "9999999";
+      popup.style.alignItems = "center";
+      popup.style.justifyContent = "center";
+      popup.style.fontSize = "14px";
+
+      // Adding hide button
+      const closePopup = document.createElement("div");
+      closePopup.style.position = "fixed";
+      closePopup.style.right = "1em";
+      closePopup.innerText = "X";
+      closePopup.onClick = function () {
+        popup.style.display = "none";
+      };
+
+      let popupInnerHTML;
+      // Adding restaurants names and links
+      if (restaurants.length === 1) {
+        popupInnerHTML = `Restaurant <b>${restaurants[0].name}</b> is available. Click <a href="${restaurants[0].url}">here</a> to order now!`;
+      } else {
+        popupInnerHTML = "The following restaurents are available: ";
+        for (let i = 0; i < restaurants.length; i++) {
+          popupInnerHTML += `<a href="${restaurants[i].url}"><b>${restaurants[i].name}</b></a>`;
+          popupInnerHTML += i !== restaurant.length - 1 ? " " : "";
+        }
+        popupInnerHTML += ". Click on the restaurant name to order now!";
+      }
+      popup.innerHTML = popupInnerHTML;
+      document.body.insertBefore(popup, document.body.firstChild);
+    }
+  });
+});
--- popup.js
+++ popup.js
@@ -1,93 +1,94 @@
-let track_button = document.getElementById("track");
-let errors_label = document.getElementById("errors");
-let restaurants_tracked = document.getElementById("restaurants_tracked");
-let restaurants = {};
+const track_button = document.getElementById("track");
+const errors_label = document.getElementById("errors");
+const tracked_restaurants = document.getElementById("tracked_restaurants");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-track_button.addEventListener("click", async () => {
+document.addEventListener("DOMContentLoaded", async function () {
+  track_button.addEventListener("click", addTrackedRestaurant);
+  await updateTrackedRestaurantsView();
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.msg === "updateTrackedRestaurantsView") {
+    console.log("updateTrackedRestaurantsView MSG");
+    updateTrackedRestaurantsView();
+  }
+});
+
+const updateTrackedRestaurantsView = async () => {
+  const restaurants = await getTrackedRestaurantsFromChromeStorage();
+  tracked_restaurants.innerHTML = "";
+  for (let i = 0; i < restaurants.length; i++) {
+    tracked_restaurants.innerHTML += i > 0 ? "<br/>" : "";
+    tracked_restaurants.innerHTML += restaurants[i].name;
+  }
+};
+
+const addTrackedRestaurant = async () => {
   try {
     let [tab] = await chrome.tabs.query({
       active: true,
       currentWindow: true,
     });
     let tab_url = tab.url;
-    let restaurant_details = {};
 
     await validateWoltURL(tab_url);
-    let restaurant_name_on_wolt = getRestaurentNameFromURL(tab_url);
-    restaurant_details = await getRestaurentDetails(restaurant_name_on_wolt);
-
-    if (!restaurant_details.online) {
-      restaurants_tracked.innerHTML =
-        restaurants_tracked.innerHTML + Object.keys(restaurants).length > 0
-          ? " " + restaurant_name_on_wolt
-          : restaurant_name_on_wolt;
-
-      restaurants[restaurant_name_on_wolt] = restaurant_details;
-      chrome.storage.sync.set({ restaurants: restaurants }, () => {});
-      restaurants[restaurant_name_on_wolt]["interval"] = setInterval(
-        checkRestaurantAvailablity,
-        30 * 1000
-      );
+    const slug = getRestaurentSlugFromURL(tab_url);
+    let restaurants = await getTrackedRestaurantsFromChromeStorage();
+    const restaurant_details = await getRestaurantDetails(slug);
+    if (restaurants.filter((r) => r.slug === slug).length > 0) {
+      throw `You're already tracking ${restaurant_details.name} availability`;
     } else {
-      alert(`${restaurant_name_on_wolt} is already online`);
-    }
-  } catch (err) {
-    errors_label.innerText = err;
-  }
-});
-
-function validateWoltURL(url) {
-  if (url.toLowerCase().indexOf("wolt.com") === -1) {
-    throw "Website is not supported";
-  }
-}
-
-function getRestaurentNameFromURL(url) {
-  return url.substring(url.lastIndexOf("/") + 1);
-}
-
-function checkRestaurantAvailablity() {
-  console.log("checking availability");
-  chrome.storage.local.get(["restaurants"], async function (results) {
-    let restaurants_names = Object.keys(results.restaurants);
-    if (restaurants_names.length > 0) {
-      for (const restaurant_name_on_wolt of restaurants_names) {
-        await fetch(
-          "https://restaurant-api.wolt.com/v3/venues/slug/" +
-            restaurant_name_on_wolt
-        )
-          .then(function (response) {
-            // The response is a Response instance.
-            // You parse the data into a useable format using `.json()`
-            return response.json();
-          })
-          .then(function (restaurant_data) {
-            if (restaurant_data.results[0].online) {
-              restaurants[restaurant_name_on_wolt]["interval"] = null;
-              chrome.tabs.sendMessage(tabs[0].id, { restaurant_name_on_wolt });
-            }
-          });
+      if (!restaurant_details.online) {
+        restaurants.push(restaurant_details);
+        await setRestaurantsOnChromeStorage(restaurants);
+        await updateTrackedRestaurantsView();
+      } else {
+        throw `${restaurant_details.name} is already online`;
       }
     }
-  });
-}
+  } catch (err) {
+    alert(err);
+  }
+};
 
-async function getRestaurentDetails(restaurant_name_on_wolt) {
+const validateWoltURL = (url) => {
+  if (
+    url.toLowerCase().indexOf("wolt.com") === -1 ||
+    url.toLowerCase().indexOf("restaurant") === -1
+  ) {
+    throw "Website is not supported";
+  }
+};
+
+const getRestaurentSlugFromURL = (url) => {
+  return url.substring(url.lastIndexOf("/") + 1);
+};
+
+const getRestaurantDetails = async (slug) => {
   let results = await fetch(
-    "https://restaurant-api.wolt.com/v3/venues/slug/" + restaurant_name_on_wolt
+    "https://restaurant-api.wolt.com/v3/venues/slug/" + slug
   )
     .then(function (response) {
-      // The response is a Response instance.
-      // You parse the data into a useable format using `.json()`
       return response.json();
     })
     .then(function (restaurant_data) {
       return {
-        name: restaurant_data.results[0].name,
+        name: restaurant_data.results[0].name[0].value,
+        slug,
+        url: restaurant_data.results[0].url
+          ? restaurant_data.results[0].url
+          : restaurant_data.results[0].public_url,
         image: restaurant_data.results[0].mainimage,
         online: restaurant_data.results[0].online,
       };
     });
   return results;
-}
+};
+
+const getTrackedRestaurantsFromChromeStorage = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(["restaurants"], function (results) {
+      resolve(results ? (results.restaurants ? results.restaurants : []) : []);
+    });
+  });
+};
